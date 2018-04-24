3049bca70fbca96a71ef1b60bc4849f3ab4c64dc
Virginia Pujols <vp2532@rit.edu>
2018-04-24 06:35:53 -0400

    fix of dataset

--- Main.py
+++ Main.py
@@ -15,9 +15,9 @@ from StopWord import StopWord
 from FileSizeScoreCalculator import FileSizeScoreCalculator
 import Metrics
 
-#SOURCE_CODE_PATH = "/Users/virginia/Documents/RIT/SEMESTER 2/SWEN 749 Evolution/Final Project"
-SOURCE_CODE_PATH = "/Users/virginia/Documents/RIT/SEMESTER 2/SWEN 749 Evolution/Final Project/code/test_source_code"
-#SOURCE_CODE_PATH = "test_source_code"
+SOURCE_CODE_PATH = "/Users/virginia/Documents/RIT/SEMESTER 2/SWEN 749 Evolution/Final Project"
+# SOURCE_CODE_PATH = "/Users/virginia/Documents/RIT/SEMESTER 2/SWEN 749 Evolution/Final Project/code/test_source_code"
+# SOURCE_CODE_PATH = "test_source_code"
 first_cap_re = re.compile('(.)([A-Z][a-z]+)')
 all_cap_re = re.compile('([a-z0-9])([A-Z])')
 porter_stemmer = PorterStemmer()
@@ -77,7 +77,6 @@ def parse_source_code():
         for filename in fnmatch.filter(file_names, "*.java"):
             java_files.append(os.path.join(root, filename))
 
-    print("java files = ", len(java_files))
     data = []
     for file in java_files:
         file_content = open(file, 'r', errors='ignore').read()
@@ -212,18 +211,18 @@ def compute_cosine_similarity(bug_report_corpus, source_code_corpus):
         file_size_score_calculator.size_list_source_code.append(sum(row))
 
 
-    # Printing results
-    # print(" -- -- Dictionary in count_vectorizer object -- -- ")
-    # print(count_vectorizer.stop_words)
-    #
-    # print(" -- -- Stop words in count_vectorizer object -- -- ")
-    # print(count_vectorizer.vocabulary_)
-    #
-    # print(' -- -- Matrix -- -- ')
-    # print(frequency_term_matrix_source_code)
-    #
-    # print(' -- -- Matrix dense -- -- ')
-    # print(frequency_term_matrix_source_code.todense())
+    #Printing results
+    #print(" -- -- Dictionary in count_vectorizer object -- -- ")
+    #print(count_vectorizer.stop_words)
+
+    #print(" -- -- Stop words in count_vectorizer object -- -- ")
+    #print(count_vectorizer.vocabulary_)
+
+    #print(' -- -- Matrix -- -- ')
+    #print(frequency_term_matrix_source_code)
+
+    print(' -- -- Matrix dense -- -- ')
+    print(frequency_term_matrix_source_code.todense())
 
     ''' Calculating tf-idf source Code'''
     # print(' -- -- tf-idf Source Code -- -- ')
@@ -244,7 +243,7 @@ def compute_cosine_similarity(bug_report_corpus, source_code_corpus):
     ''' Cosine Similarity '''
     # print(' -- -- Cosine Similarity -- -- ')
     cos_simi = cosine_similarity(tf_idf_matrix_bug_report, tf_idf_matrix_source_code)
-    # print(cos_simi)
+    print(cos_simi)
 
     ''' Associate similarity score with corresponding source code '''
     similarity_score = cos_simi[0]
@@ -351,19 +350,22 @@ def get_bug_report_cosine_similarity(dataset, source_code_list, cos_simi, max_wo
 
 
 def save_ranks_to_file(bug_report, dataset):
-    # rVSM - SemiScore - FinalScore - FileName"
     ranks_file.write("\n +++++++++++++++++++++++++++++++++++++ \n")
     ranks_file.write("Bug ID: ")
     ranks_file.write(bug_report.id)
     ranks_file.write("\n")
 
     rank_first_file = -1
+    files_binary_relevance = []
     did_locate_bug = False
     count = 0
 
     for key, value in sorted(dataset.items(), key=lambda e: e[1][DataSetField.final_rank], reverse=True):
+        # rVSM - SemiScore - FinalScore - FileName"
+        print(value, key)
+
         count += 1
-        if count > 20:
+        if count > 10:  # 20
             break
 
         ranked = False
@@ -374,16 +376,18 @@ def save_ranks_to_file(bug_report, dataset):
         top = " 20 "
         if count == 1:
             top = " 1 "
-        elif count > 1 <= 5:
+        elif 1 < count <= 5:
             top = " 5 "
-        elif count > 5 <= 10:
+        elif count > 5:
             top = " 10 "
 
-        ranks_file.write("RANKED " + top + " | " if ranked else "")
+        ranks_file.write("TOP " + top + " | " if ranked else "")
         ranks_file.write(" | ".join(str(i) for i in value))
         ranks_file.write(" ")
         ranks_file.write(key)
         ranks_file.write("\n")
+
+        files_binary_relevance.append(1 if ranked else 0)
 
         if not did_locate_bug and ranked and count <= 10:
             did_locate_bug = True
@@ -393,7 +397,36 @@ def save_ranks_to_file(bug_report, dataset):
     ranks_file.write("\n")
     ranks_file.write("SUCCESSFULLY LOCATED BUG! " if did_locate_bug else "")
     ranks_file.write("\n +++++++++++++++++++++++++++++++++++++ \n")
-    return rank_first_file
+    return rank_first_file, files_binary_relevance
+
+
+def localize_bugs(current_bug_report, source_code_list, bug_report_list, dataset):
+    bug_report_corpus = current_bug_report.content_corpus
+    source_code_corpus = []
+
+    max_file_word_count, min_file_word_count = get_max_min_file_size(source_code_corpus, source_code_list)
+
+    query = bug_report_corpus
+    documents = source_code_corpus
+
+    # 0: Cosine Similarity (VSM)
+    cosine_similarity = compute_cosine_similarity(query, documents)
+
+    # 1: Cosine Similarity for a bug report with source code file size (rVSM)
+    rVSMz_max, rVSMz_min = get_bug_report_cosine_similarity(dataset, source_code_list, cosine_similarity,
+                                                            max_file_word_count, min_file_word_count)
+
+    # 2: Cosine Similarity for a bug report with the rest of bug reports (SIMI_SCORE)
+    semi_score_max, semi_score_min = get_similar_bug_report_cosine_similarity(dataset, current_bug_report,
+                                                                              bug_report_list)
+
+    # 3: Combine 1 and 2
+    combine_ranks(0.2, dataset, rVSMz_min, rVSMz_max, semi_score_min, semi_score_max)
+
+    # print results in file
+    first_file_pos_ranked, files_binary_relevance = save_ranks_to_file(current_bug_report, dataset)
+    return first_file_pos_ranked, files_binary_relevance
+
 
 def main():
     # This is the dataset structure {file_path, [rVSMScore - simiScore - Final Rank]}
@@ -402,43 +435,42 @@ def main():
 
     source_code_list = parse_source_code()
     bug_report_list = parse_bug_reports()
+    print("JAVA FILES  = ", len(source_code_list))
+    print("BUG REPORTS = ", len(bug_report_list))
 
     # metrics
     files_pos_ranked = []
+    binary_relevance_list = []
 
+    # FORM 1
     # Compute for each bug report
-    for i in range(len(bug_report_list)):
-        current_bug_report = bug_report_list[i]
-        bug_report_corpus = current_bug_report.content_corpus
-        source_code_corpus = []
+    # for i in range(len(bug_report_list)):
+    #     current_bug_report = bug_report_list[i]
+    #     dataset = {}
+    #
+    #     first_file_pos_ranked, files_binary_relevance = localize_bugs(current_bug_report, source_code_list, bug_report_list, dataset)
+    #
+    #     files_pos_ranked.append(first_file_pos_ranked)
+    #     binary_relevance_list.append(files_binary_relevance)
+    # END FORM 1
 
-        max_file_word_count, min_file_word_count = get_max_min_file_size(source_code_corpus, source_code_list)
+    # FORM 2
+    # Compute for ONE bug report
+    current_bug_report = bug_report_list[0]
+    first_file_pos_ranked, files_binary_relevance = localize_bugs(current_bug_report, source_code_list, bug_report_list,
+                                                                  dataset)
 
-        query = bug_report_corpus
-        documents = source_code_corpus
-
-        # 0: Cosine Similarity (VSM)
-        cosine_similarity = compute_cosine_similarity(query, documents)
-
-        # 1: Cosine Similarity for a bug report with source code file size (rVSM)
-        rVSMz_max, rVSMz_min = get_bug_report_cosine_similarity(dataset, source_code_list, cosine_similarity,
-                                                                max_file_word_count, min_file_word_count)
-
-        # 2: Cosine Similarity for a bug report with the rest of bug reports (SIMI_SCORE)
-        semi_score_max, semi_score_min = get_similar_bug_report_cosine_similarity(dataset, current_bug_report, bug_report_list)
-
-        # 3: Combine 1 and 2
-        combine_ranks(0.2, dataset, rVSMz_min, rVSMz_max, semi_score_min, semi_score_max)
-
-        # print results in file
-        first_file_pos_ranked = save_ranks_to_file(current_bug_report, dataset)
-        files_pos_ranked.append(first_file_pos_ranked)
+    files_pos_ranked.append(first_file_pos_ranked)
+    binary_relevance_list.append(files_binary_relevance)
+    # END FORM 2
 
     # METRICS....
     mean_reciprocal_rank = Metrics.mean_reciprocal_rank(files_pos_ranked, len(bug_report_list))
+    mean_average_precision = Metrics.mean_average_precision(binary_relevance_list)
 
     print("---- METRICS ---- ")
-    print("MRR (Mean Reciprocal Rank) = ", mean_reciprocal_rank)
+    print("MRR (Mean Reciprocal Rank)   = ", mean_reciprocal_rank)
+    print("MAP (Mean Average Precision) = ", mean_average_precision)
 
     ranks_file.close()
     e = int(time.time() - start_time)
